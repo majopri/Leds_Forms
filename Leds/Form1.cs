86535cc3d432bf93572386ce2131df90c1d3b036
majopri <166362054+majopri@users.noreply.github.com>
2025-10-20 14:25:45 -0300

    Add files via upload

---
+++ Leds/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Leds
+{
+    public partial class Form1 : Form
+    {
+        private Leds Luz;
+        public Form1()
+        {
+            InitializeComponent();
+            Luz = new Leds();
+            tbx_IndicarValor.Text = Luz.Getdados().ToString();
+            Atualizainterface();
+        }
+
+        private void Atualizainterface()
+        {
+            tbx_IndicarValor.Text = Luz.Getdados().ToString();
+            tbx_dado_2.Text = Convert.ToString(Luz.Getdados(), 2);
+            ckb_led1.Checked = Luz.Getled(1);
+            ckb_led2.Checked = Luz.Getled(2);
+            ckb_led3.Checked = Luz.Getled(3);
+            ckb_led4.Checked = Luz.Getled(4);
+            ckb_led5.Checked = Luz.Getled(5);
+            ckb_led6.Checked = Luz.Getled(6);
+            ckb_led7.Checked = Luz.Getled(7);
+            ckb_led8.Checked = Luz.Getled(8);
+        }
+
+            private void btn_on_1_Click(object sender, EventArgs e)
+        {
+            Luz.Acender(1);
+            Atualizainterface();
+        }
+
+        private void btn_off_1_Click(object sender, EventArgs e)
+        {
+            Luz.Apagar(1);
+            Atualizainterface();
+        }
+
+        private void btn_on_2_Click(object sender, EventArgs e)
+        {
+            Luz.Acender(2);
+            Atualizainterface();
+        }
+
+        private void btn_off_2_Click(object sender, EventArgs e)
+        {
+            Luz.Apagar(2);
+            Atualizainterface();
+        }
+
+        private void btn_on_3_Click(object sender, EventArgs e)
+        {
+            Luz.Acender(3);
+            Atualizainterface();
+        }
+
+        private void btn_off_3_Click(object sender, EventArgs e)
+        {
+            Luz.Apagar(3);
+            Atualizainterface();
+        }
+
+        private void btn_on_4_Click(object sender, EventArgs e)
+        {
+            Luz.Acender(4);
+            Atualizainterface();
+        }
+
+        private void btn_off_4_Click(object sender, EventArgs e)
+        {
+            Luz.Apagar(4);
+            Atualizainterface();
+        }
+
+        private void btn_on_5_Click(object sender, EventArgs e)
+        {
+            Luz.Acender(5);
+            Atualizainterface();
+        }
+
+        private void btn_off_5_Click(object sender, EventArgs e)
+        {
+            Luz.Apagar(5);
+            Atualizainterface();
+        }
+
+        private void btn_on_6_Click(object sender, EventArgs e)
+        {
+            Luz.Acender(6);
+            Atualizainterface();
+        }
+
+        private void btn_off_6_Click(object sender, EventArgs e)
+        {
+            Luz.Apagar(6);
+            Atualizainterface();
+        }
+
+        private void btn_on_7_Click(object sender, EventArgs e)
+        {
+            Luz.Acender(7);
+            Atualizainterface();
+        }
+
+        private void btn_off_7_Click(object sender, EventArgs e)
+        {
+            Luz.Apagar(7);
+            Atualizainterface();
+        }
+
+        private void btn_on_8_Click(object sender, EventArgs e)
+        {
+            Luz.Acender(8);
+            Atualizainterface();
+        }
+
+        private void btn_off_8_Click(object sender, EventArgs e)
+        {
+            Luz.Apagar(8);
+            Atualizainterface();
+        }
+    }
+}
---
+++ Leds/Leds.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leds
+{
+    internal class Leds
+    {
+        private byte dado;
+
+        public Leds()
+        {
+            this.dado = 0;
+        }
+        public Leds(byte dado)
+        {
+            this.dado = dado;
+        }
+        public void Acender(int led)
+        {
+            dado = (byte)((int)dado | (int)(Math.Pow(2, (led - 1))));
+        }
+        public void Apagar(int led)
+        {
+            dado = (byte)((int)dado & (255 - (int)(Math.Pow(2, (led - 1)))));
+        }
+
+        public byte Getdados()
+        {
+            return dado;
+        }
+
+        public bool Getled(int led)
+        {
+            byte teste;
+            teste = (byte)((int)dado & (int)(Math.Pow(2, (led - 1))));
+            return (teste > 0);
+
+        }
+    }
+}
